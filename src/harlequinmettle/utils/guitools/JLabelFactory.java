7f2ee87ccce888a8e92b00dde9de49f1161da290
Andrew Parelius <parelius@uw.edu>
2014-06-18 12:36:00 -0500

    added some jlabel options; scrollmenu from internet unused tho

--- src/harlequinmettle/utils/guitools/JLabelFactory.java
+++ src/harlequinmettle/utils/guitools/JLabelFactory.java
@@ -1,6 +1,7 @@
 package harlequinmettle.utils.guitools;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 
@@ -9,6 +10,26 @@ public class JLabelFactory {
 		JLabel general = new JLabel(string);
 		general.setOpaque(true);
 		general.setBackground(new Color(100, 200, 250));
+		return general;
+	}
+	public static JLabel doLightBluishJLabel(String string) {
+		JLabel general = new JLabel(string);
+		general.setOpaque(true);
+		general.setBackground(new Color(150, 230, 250));
+		return general;
+	}
+	public static JLabel doBluishJLabel(String string,Font font) {
+		JLabel general = new JLabel(string);
+		general.setFont(font);
+		general.setOpaque(true);
+		general.setBackground(new Color(100, 200, 250));
+		return general;
+	}
+	public static JLabel doLightBluishJLabel(String string, Font font) {
+		JLabel general = new JLabel(string);
+		general.setFont(font);
+		general.setOpaque(true);
+		general.setBackground(new Color(150, 230, 250));
 		return general;
 	}
 }
--- src/harlequinmettle/utils/guitools/VerticalJPanel.java
+++ src/harlequinmettle/utils/guitools/VerticalJPanel.java
@@ -5,7 +5,10 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 public class VerticalJPanel extends JPanel{
-public VerticalJPanel(){ 
-	setLayout(new GridLayout(0, 1));
-}
+	public VerticalJPanel(){ 
+		setLayout(new GridLayout(0, 1));
+	}
+	public VerticalJPanel(int columns){ 
+		setLayout(new GridLayout(0,columns));
+	}
 }
